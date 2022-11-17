28c9aa112e9173e7b40eb982e1fbf6a13e20dbab
MarianoReyes <josemarianoreyesh02@gmail.com>
2022-11-17 08:33:36 -0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -6,5 +6,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('js');
     eleventyConfig.addPassthroughCopy('assets');
+    eleventyConfig.addPassthroughCopy('admin');
 
 }
